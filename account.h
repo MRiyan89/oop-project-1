975dc534d152b712d7027a5190f08ecd20638496
Riyan Ahmed <bscs24089@itu.edu.pk>
2025-06-12 03:47:11 +0500

    account header file

---
+++ account.h
@@ -0,0 +1,18 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+#include <iostream>
+#include "str.h"
+#include "global.h"
+using namespace std;
+class Account{
+public:
+    str account_name, account_type, account_id, account_password;
+    int account_no = 0 ,book_no = 0;
+    int book[10];
+    void addAccount();
+    void deleteAccount(Account* &);
+    void searchAccount(Account* &);
+    void modifyAccount(Account* &);
+    void showAllAccount();
+};
+#endif
